5e77a4c2609d2b2697f7306f6489c098f10c63e0
Hatchino <hemme.lea@gmail.com>
2023-01-04 16:07:26 +0100

    mini-project calculate imc

--- imc.js
+++ imc.js
@@ -12,15 +12,15 @@ const imc = calcIMC(taille, poids);
 alert('Votre IMC est de : ' + imc + '.');
 
 if (imc < 18.5) {
-    alert("Selon votre IMC, vous être trop maigre");
+    alert("Selon votre IMC, vous êtes trop maigre");
 } else if ((imc >= 18.5) && (imc <= 24.9)) {
-    alert("Selon votre IMC, vous être normal");
+    alert("Selon votre IMC, vous êtes normal");
 } else if ((imc >= 25) && (imc <= 29.9)) {
-    alert("Selon votre IMC, vous être en surpoids");
+    alert("Selon votre IMC, vous êtes en surpoids");
 } else if ((imc >= 30) && (imc <= 34.9)) {
-    alert("Selon votre IMC, vous être obèse");
+    alert("Selon votre IMC, vous êtes obèse");
 } else if (imc >= 35) {
-    alert("Selon votre IMC, vous être en obésité sévère");
+    alert("Selon votre IMC, vous êtes en obésité sévère");
 }
 
 
